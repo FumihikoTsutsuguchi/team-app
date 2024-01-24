cd451a67eaa3e61c979f8beecb1916e8fb33d147
kaxia7216 <kaxia.khlove@gmail.com>
2024-01-25 07:09:06 +0900

    getAvatarId(), setAvatarIdToPlayers()の作成(avatars.php) 接続情報、誤記の修正(control.php) extension項目の修正(php.ini)

--- app/php/avatars.php
+++ app/php/avatars.php
@@ -1,36 +1,51 @@
 <?php
 
-function selectAvatars()
+function getAvatarId($get)
 {
     $pdo = dbConnect();
-
+    $pdo->beginTransaction();
     try {
-        $query = file_get_contents('select.sql');
+        $query = 'SELECT avatar_id FROM avatars WHERE file_name = :avatarName';
         $statement = $pdo->prepare($query);
+        $statement->bindValue(':avatarName', $post['avatar'] . '.png', PDO::PARAM_STR);
         $statement->execute();
+        $pdo->commit();
 
-        return true;
+        return $statement->fetch(PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
         echo "取得失敗";
-        return false;
+        return $e;
     } finally {
         // 処理なし
     }
 }
 
-function renewAvatars()
+function setAvatarIdToPlayers($post)
 {
     $pdo = dbConnect();
 
-    try {
-        $query = file_get_contents('renew.sql');
-        $statement = $pdo->prepare($query);
-        $statement->execute();
+    //トランザクション開始
+    $pdo->beginTransaction();
 
-        return true;
+    try {
+        //設定したいアバターのidを取得
+        $query = 'SELECT avatar_id FROM avatars WHERE file_name = :avatarName';
+        $statement = $pdo->prepare($query);
+        $statement->bindValue(':avatarName', $post['avatar'] . '.png', PDO::PARAM_STR);
+        $statement->execute();
+        $avatar_id = $statement->fetch(PDO::FETCH_ASSOC);
+
+        //取得したavatar_idをplayersへ格納
+        $query = 'UPDATE players SET current_avatar_id = :avatar_id';
+        $statement2 = $pdo->prepare($query);
+        $statement2->bindValue(':avatar_id', $avatar_id, PDO::PARAM_INT);
+        $statement2->execute();
+
+        //トランザクションをコミット
+        $pdo->commit();
     } catch (PDOException $e) {
+        $pdo->rollback();
         echo "更新失敗";
-        return false;
     } finally {
         // 処理なし
     }
