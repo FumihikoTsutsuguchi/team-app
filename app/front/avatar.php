e59339540fb3e7578f996b5ce183b65c5add759e
FumihikoTsutsuguchi <fumisamu927@gmail.com>
2024-01-25 21:23:26 +0900

    [update]アバターページ内のpost処理追加・スタイルの調整

--- app/front/avatar.php
+++ app/front/avatar.php
@@ -1,5 +1,14 @@
 <?php require_once("./header.php"); ?>
 
+<?php
+if ($_POST == []) {
+    //処理なし
+} else {
+    setAvatarIdToPlayers($_POST);
+    $selectedAvatar = isset($_POST['avatar']) ? $_POST['avatar'] : '';
+}
+?>
+
 <div class="c-wrapper">
     <div class="c-nav-breadcrumb">
         <ol>
@@ -12,7 +21,7 @@
         <div class="p-avatar__wrap">
             <ul class="p-avatar__wrap-list">
                 <li>
-                    <button name="avatar" value="king" class="c-avatar">
+                    <button name="avatar" value="king" class="c-avatar <?php echo ($selectedAvatar === 'king') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
                             <img src="./img/avatar/king.png" alt="">
                         </div>
@@ -20,7 +29,7 @@
                     </button>
                 </li>
                 <li>
-                    <button name="avatar" value="tengu" class="c-avatar">
+                    <button name="avatar" value="tengu" class="c-avatar <?php echo ($selectedAvatar === 'tengu') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
                             <img src="./img/avatar/tengu.png" alt="">
                         </div>
@@ -28,7 +37,7 @@
                     </button>
                 </li>
                 <li>
-                    <button name="avatar" value="kappa" class="c-avatar">
+                    <button name="avatar" value="kappa" class="c-avatar <?php echo ($selectedAvatar === 'kappa') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
                             <img src="./img/avatar/kappa.png" alt="">
                         </div>
@@ -38,15 +47,15 @@
             </ul>
             <ul class="p-avatar__wrap-list">
                 <li>
-                    <button name="avatar" value="gitHubNeko" class="c-avatar">
+                    <button name="avatar" value="gitHubNeko" class="c-avatar <?php echo ($selectedAvatar === 'gitHubNeko') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
-                            <img src="./img/avatar/cat.png" alt="">
+                            <img src="./img/avatar/gitHubNeko.png" alt="">
                         </div>
                         <p>G◯tHubネコ</p>
                     </button>
                 </li>
                 <li>
-                    <button name="avatar" value="capybara" class="c-avatar">
+                    <button name="avatar" value="capybara" class="c-avatar <?php echo ($selectedAvatar === 'capybara') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
                             <img src="./img/avatar/capybara.png" alt="">
                         </div>
@@ -54,23 +63,23 @@
                     </button>
                 </li>
                 <li>
-                    <button name="avatar" value="dockerWhale" class="c-avatar">
+                    <button name="avatar" value="dockerWhale" class="c-avatar <?php echo ($selectedAvatar === 'dockerWhale') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
-                            <img src="./img/avatar/whale.png" alt="">
+                            <img src="./img/avatar/dockerWhale.png" alt="">
                         </div>
                         <p>D◯ckerくじら</p>
                     </button>
                 </li>
                 <li>
-                    <button name="avatar" value="phpElephant" class="c-avatar">
+                    <button name="avatar" value="phpElephant" class="c-avatar <?php echo ($selectedAvatar === 'phpElephant') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
-                            <img src="./img/avatar/elephant.png" alt="">
+                            <img src="./img/avatar/phpElephant.png" alt="">
                         </div>
                         <p>P◯Pゾウ</p>
                     </button>
                 </li>
                 <li>
-                    <button name="avatar" value="alpaca" class="c-avatar">
+                    <button name="avatar" value="alpaca" class="c-avatar <?php echo ($selectedAvatar === 'alpaca') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
                             <img src="./img/avatar/alpaca.png" alt="">
                         </div>
@@ -78,7 +87,7 @@
                     </button>
                 </li>
                 <li>
-                    <button name="avatar" value="dog" class="c-avatar">
+                    <button name="avatar" value="dog" class="c-avatar <?php echo ($selectedAvatar === 'dog') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
                             <img src="./img/avatar/dog.png" alt="">
                         </div>
@@ -88,23 +97,23 @@
             </ul>
             <ul class="p-avatar__wrap-list">
                 <li>
-                    <button name="avatar" value="officeWorker1" class="c-avatar">
+                    <button name="avatar" value="officeWorker1" class="c-avatar <?php echo ($selectedAvatar === 'officeWorker1') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
-                            <img src="./img/avatar/office-worker_1.png" alt="">
+                            <img src="./img/avatar/officeWorker1.png" alt="">
                         </div>
                         <p>サラリーマン1</p>
                     </button>
                 </li>
                 <li>
-                    <button name="avatar" value="officeWorker2" class="c-avatar">
+                    <button name="avatar" value="officeWorker2" class="c-avatar <?php echo ($selectedAvatar === 'officeWorker2') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
-                            <img src="./img/avatar/office-worker_2.png" alt="">
+                            <img src="./img/avatar/officeWorker2.png" alt="">
                         </div>
                         <p>サラリーマン2</p>
                     </button>
                 </li>
                 <li>
-                    <button name="avatar" value="student" class="c-avatar">
+                    <button name="avatar" value="student" class="c-avatar <?php echo ($selectedAvatar === 'student') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
                             <img src="./img/avatar/student.png" alt="">
                         </div>
@@ -112,7 +121,7 @@
                     </button>
                 </li>
                 <li>
-                    <button name="avatar" value="child" class="c-avatar">
+                    <button name="avatar" value="child" class="c-avatar <?php echo ($selectedAvatar === 'child') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
                             <img src="./img/avatar/child.png" alt="">
                         </div>
@@ -120,7 +129,7 @@
                     </button>
                 </li>
                 <li>
-                    <button name="avatar" value="windowsXpDolphin" class="c-avatar">
+                    <button name="avatar" value="windowsXpDolphin" class="c-avatar <?php echo ($selectedAvatar === 'windowsXpDolphin') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
                             <img src="./img/avatar/dolphin.png" alt="">
                         </div>
@@ -130,7 +139,7 @@
             </ul>
             <ul class="p-avatar__wrap-list">
                 <li>
-                    <button name="avatar" value="primitiveMan" class="c-avatar">
+                    <button name="avatar" value="primitiveMan" class="c-avatar <?php echo ($selectedAvatar === 'primitiveMan') ? 'selected' : ''; ?>">
                         <div class="c-avatar__img">
                             <img src="./img/avatar/primitive-man.png" alt="">
                         </div>
